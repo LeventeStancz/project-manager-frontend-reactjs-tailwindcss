63b0e1b07bc33b0a4126fb46ea21de5f2caca37b
LeventeStancz <stancz.levente0304@gmail.com>
2023-08-03 12:15:29 +0200

    feat: auth context

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  //creating state to store auth values
+  const [auth, setAuth] = useState(false);
+  //for "trust this device" function
+  const [trustedDevice, setTrustedDevice] = useState(
+    JSON.parse(localStorage.getItem("trustedDevice")) || false
+  );
+
+  //passing state with provider to children components
+  return (
+    <AuthContext.Provider
+      value={{ auth, setAuth, trustedDevice, setTrustedDevice }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
