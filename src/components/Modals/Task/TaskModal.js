dfc90d62e08a86c7865792c0ea720cbd65b85b0f
LeventeStancz <stancz.levente0304@gmail.com>
2023-08-12 22:34:05 +0200

    fix: fixed modal closing when clicked inside

--- src/components/Modals/Task/TaskModal.js
+++ src/components/Modals/Task/TaskModal.js
@@ -6,37 +6,56 @@ import ReactDOM from "react-dom";
 
 const TaskModal = ({ show, closeModal, task }) => {
   if (!show) return null;
+  const handleOnClose = (e) => {
+    if (e.target.id === "container") closeModal();
+  };
   return ReactDOM.createPortal(
-    <div className="overflow-y-scroll overflow-x-hidden">
-      <div
-        onClick={closeModal}
-        className="h-screen w-full fixed top-0 backdrop-blur-sm"
-      ></div>
-      <div className="h-fit w-fit fixed top-20 left-1/2 -translate-x-1/2 z-50 flex justify-center items-start">
-        <div className="w-fit min-w-[740px] max-h-full p-6 z-50 bg-custom-gray-base rounded-2xl">
-          <div className="flex items-center justify-between mb-4">
-            <div className="w-full flex items-center gap-x-4 text-lg">
-              <h1 className="w-full text-3xl font-bold text-custom-blue">
-                Details about the task:
-              </h1>
-              <div className="w-full flex justify-evenly">
-                <DateBox date={task?.deadline} />
-                <PriorityBox priority={task?.priority} />
-              </div>
+    <div
+      id="container"
+      onClick={handleOnClose}
+      className="h-screen fixed top-0 w-full backdrop-blur-sm flex flex-col items-center justify-center overflow-hidden p-8"
+    >
+      <div className="p-6 w-2/4 min-w-[800px] bg-custom-gray-base rounded-2xl overflow-y-auto overflow-x-hidden">
+        <div className="flex items-center justify-between mb-4">
+          <div className="w-full flex items-center gap-x-4 text-lg">
+            <h1 className="w-full text-3xl font-bold text-custom-blue">
+              Details about the task:
+            </h1>
+            <div className="text-xl w-full flex justify-evenly">
+              <DateBox date={task?.deadline} />
+              <PriorityBox priority={task?.priority} />
             </div>
-            <p
-              onClick={closeModal}
-              className="text-2xl text-custom-red font-bold hover:cursor-pointer"
-            >
-              <XCircleIcon className="h-9 w-9" />
-            </p>
           </div>
-          <div className="w-full py-2 flex flex-col gap-y-3">
+          <p
+            onClick={closeModal}
+            className="text-2xl text-custom-red font-bold hover:cursor-pointer"
+          >
+            <XCircleIcon className="h-9 w-9" />
+          </p>
+        </div>
+        <div className="w-full py-2 flex flex-col gap-y-4">
+          <h1 className="text-2xl font-semibold px-1 text-custom-purple">
+            Title:
+          </h1>
+          <div className="flex flex-col gap-y-2">
+            <HorizontalLine />
             <h1 className="text-2xl px-1">{task?.title}</h1>
+            <HorizontalLine />
+          </div>
+          <h1 className="text-2xl font-semibold px-1 text-custom-purple">
+            Short description:
+          </h1>
+          <div className="flex flex-col gap-y-2">
             <HorizontalLine />
             <h2 className="text-xl px-1">{task?.shortDescription}</h2>
             <HorizontalLine />
-            <h2 className="text-xl px-1">
+          </div>
+          <h1 className="text-2xl font-semibold px-1 text-custom-purple">
+            Description:
+          </h1>
+          <div className="flex flex-col gap-y-2">
+            <HorizontalLine />
+            <h2 className="text-xl px-1 leading-relaxed">
               {task?.description
                 ? task?.description
                 : "No further information..."}
--- src/layouts/ProjectLayout/TaskLayout/TaskCard.js
+++ src/layouts/ProjectLayout/TaskLayout/TaskCard.js
@@ -28,13 +28,13 @@ const TaskCard = ({ task, index }) => {
               className={
                 (snapshot.isDragging &&
                   " outline outline-2 outline-custom-blue ") +
-                " min-w-[320px] w-fit max-w-[360px] min-h-[120px] h-fit max-h-[180px] bg-custom-gray-base rounded-xl flex flex-row flex-nowrap hover:cursor-pointer"
+                " w-[360px] h-fit bg-custom-gray-base rounded-xl flex flex-row flex-nowrap hover:cursor-pointer"
               }
             >
               <div>
                 <div className={bgColor() + " w-6 h-full rounded-l-xl"}></div>
               </div>
-              <div className="w-full flex flex-col p-3 gap-y-3">
+              <div className="h-full w-full flex flex-col p-3 gap-y-3">
                 <div className="w-full flex flex-col gap-y-2 justify-center">
                   <h1 className="text-xl font-semibold">{task.title}</h1>
                   <h2 className="text-custom-gray-bright font-medium">
@@ -46,7 +46,6 @@ const TaskCard = ({ task, index }) => {
                   <PriorityBox priority={task?.priority} />
                 </div>
               </div>
-
               {provided.placeholder}
             </div>
           );
